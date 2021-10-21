e05cb9e4c52064c17931f2a79213004f940e24ee
SEN-SENSATION <prawich.th@gmail.com>
2021-10-21 18:58:46 +0700

    add Github Page and Warning Modal

---
+++ Test Version/asset/script/main.js
@@ -0,0 +1,28 @@
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector("#btn");
+const searchBtn = document.querySelector(".bx-search");
+const menuBtnChange =  function() {
+  if(sidebar.classList.contains("open")){
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+  }else {
+    closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+  }
+}
+const startUp = function () {
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+}
+
+startUp();
+
+      closeBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        menuBtnChange();//calling the function(optional)
+      });
+    
+      searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+        sidebar.classList.toggle("open");
+        menuBtnChange(); //calling the function(optional)
+      });
+    
+      // following are the code to change sidebar button(optional)
